058c419137105e634f9918663ca2c6598fffd8f3
Tan Jia Le <tan.jia.le.98@gmail.com>
2019-01-24 23:20:34 +0800

    Added function to parse the profiles files Uploaded sample profiles Added .gitignore

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+
+"""
+Dictionary mapping name to a dictionary of profile information
+"""
+profiles = {}
+
+"""
+Pretty prints a dictionary
+"""
+def prettyprint(dictionary):
+    print json.dumps(dictionary, sort_keys=False, indent=4)
+
+
+"""
+Takes in a file, parses the different fields and updates the profiles dictionary
+"""
+def parse(doc):
+    lines = [i.strip() for i in doc.readlines()]
+    name = lines[0][len("Name:"):].strip()
+    gender = lines[1][len("Gender:"):].strip()
+    if gender in ["Male", "M"]:
+        gender = "M"
+    else:
+        gender = "F"
+    country = lines[2][len("Country:"):].strip()
+    acceptable_country = [i.strip() for i in lines[3][len("Acceptable_country:"):].strip().split(',')]
+    age = lines[4][len("Age:"):].strip()
+    acceptable_age_range = lines[5][len("Acceptable_age_range:"):].strip().split('-')
+    likes = [i.strip() for i in lines[6][len("Likes:"):].strip().split(',') if i != ""]
+    dislikes = [i.strip() for i in lines[7][len("Dislikes:"):].strip().split(',') if i != ""]
+    books = [i.strip() for i in lines[10:] if i != ""]
+    profiles[name] = {
+        "gender": gender[0],
+        "country": country,
+        "acceptable_country": acceptable_country,
+        "age": int(age),
+        "acceptable_age_range": {
+            "start": int(acceptable_age_range[0]),
+            "end": int(acceptable_age_range[1]) + 1,
+        },
+        "likes": likes,
+        "dislikes": dislikes,
+        "books": books
+    }
+
+
+if __name__ == "__main__":
+    directory = ".\\profiles"
+    files = os.listdir(directory)
+    for filename in files:
+        with open(os.path.join(directory,files[0]),"r") as f:
+            parse(f)
+            f.close()
+
+    prettyprint(profiles)
+
+
