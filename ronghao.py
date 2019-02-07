f1218e703bef6744d26337b836ffaad1db9e3f9f
qazpl9 <tanronghao@yahoo.com.sg>
2019-02-07 20:42:30 +0800

    new1

--- ronghao.py
+++ ronghao.py
@@ -2,32 +2,48 @@ import getfilepath
 from os.path import isdir
 from fileparser import pretty_print
 
-
-def viewMatchesOverall():
+#This function is the first function to be call from the main and 
+def viewMatchesOverall(profile):
+    #query is to take in name input by the user 
     query = raw_input("Write the name of the person you want to know for the top 3 matches: ")
-    print overallCompatibility(query)
 
-directory = getfilepath.getFilePath()
-profiles = getfilepath.getProfiles(directory)
+    #profiles will be used as a variable for easier identification
+    profiles = profile
+    
+    #global profiles allow all the functions in the module to use profiles.
+    global profiles
+    print overallCompatibility(query)
 
 #list all name
 def listAllName():
    return profiles.keys()
         
-a = "Carol"
-#Return Person X country
-def country(x):
-    return profiles[str(x)]["country"]
+
+#Return Person country
+def country(person):
+    return profiles[str(person)]["country"]
 
 #If country is someone else acceptable country, give 25 points to that person
 def countryCompatibility(x):
-    CC = {}
-    for each in profiles.keys():
-        CC[each] = 0
-        if each != str(x):
-            for each2 in profiles[each]['acceptable_country']:
-                if (profiles[str(x)]["country"] ==  each2) :
-                    CC[each] = 25
+    
+    #initialise countryCompat as a dictionary
+    Country_compat_dict = {}
+    
+    #For each individual profiles dictionary with name as the key
+    for name in profiles.keys():
+
+        #Give everyone a starting points of 0 by using a dict whereby "name": points
+        Country_compat_dict[each] = 0
+
+        #if the person input by the user is found in the list of profile, do nothing so as to avoid giving himself points
+        if name != str(x):
+
+            #Compare everyone country with the person input by the user acceptable country
+            for acc_country in profiles[name]['acceptable_country']:
+
+                #if their country is also the person acceptable country, add 25 points to that individual
+                if (profiles[str(x)]["country"] ==  acc_country) :
+                    Country_compat_dict[name] = 25
             
     return CC
 
@@ -37,13 +53,25 @@ def age(x):
 
 #If age is someone else Acceptable age range, give 25 points to that person
 def ageCompatibility(x):
-    AC = {}
-    for each in profiles.keys():
-        AC[each] = 0
-        if each != str(x):
-            for each2 in (range((profiles[each]['acceptable_age_range']['start']),(profiles[each]['acceptable_age_range']['end']))):
-                if (profiles[str(x)]["age"] ==  each2) :
-                    AC[each] = 25
+
+    #initialise Age_comp as a dictionary
+    Age_comp_dict = {}
+
+    #For each individual profiles dictionary with name as the key
+    for name in profiles.keys():
+
+        #Give everyone a starting points of 0 by using a dict whereby "name": points
+        Age_comp_dict[name] = 0
+
+        #if the person input by the user is found in the list of profile, do nothing so as to avoid giving himself points
+        if name != str(x):
+
+            #Compare everyone's age with the person's acceptable age range
+            for acc_Age_Range in (range((profiles[name]['acceptable_age_range']['start']),(profiles[name]['acceptable_age_range']['end']))):
+
+                #if their age is in the person acceptable age range, add 25 points to that individual
+                if (profiles[str(x)]["age"] ==  acc_Age_Range) :
+                    Age_comp_dict[name] = 25
 
     return AC
             
